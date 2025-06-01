085cdb7a2aff7a81a8f243da812478463dc7483c
adept-fray <siavash.kh41@gmail.com>
2025-06-01 16:55:52 +0200

    Completed the LibraryApp.

---
+++ 01-CSharp-Basics/LibraryApp/LibraryApp/Library.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApp
+{
+    internal class Library
+    {
+        List<Book> list = new List<Book>();
+
+        public void AddBook(Book book)
+        {
+            list.Add(book);
+        }
+        public void RemoveBook(Book book)
+        {
+            list.Remove(book);
+        }
+
+        public void PrintBooks()
+        {
+            foreach (Book book in list)
+            {
+                book.PrintInfo();
+            }
+        }
+    }
+}
--- 01-CSharp-Basics/LibraryApp/LibraryApp/Program.cs
+++ 01-CSharp-Basics/LibraryApp/LibraryApp/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LibraryApp;
+
+Library library = new Library();
+
+Book book = new Book("ok", "okauthor", DateOnly.Parse("2025-02-02"));
+
+library.AddBook(book);
+
+Book book1 = new Book("Title new", "Author new", DateOnly.Parse("2025-02-02"));
+library.AddBook(book1);
+
+
+Book book2 = new Book("nsoo", "Author nsoo", DateOnly.Parse("2025-02-02"));
+library.AddBook(book2);
+
+
+library.PrintBooks();
