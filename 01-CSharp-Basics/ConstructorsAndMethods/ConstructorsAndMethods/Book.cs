e6fea17f7e1a05c91e0a895a1509085d0b2cf523
adept-fray <siavash.kh41@gmail.com>
2025-06-01 17:30:53 +0200

    Encapsulation & Access Modifiers.

--- 01-CSharp-Basics/ConstructorsAndMethods/ConstructorsAndMethods/Book.cs
+++ 01-CSharp-Basics/ConstructorsAndMethods/ConstructorsAndMethods/Book.cs
@@ -12,25 +12,45 @@ namespace ConstructorsAndMethods
         public string Author { get; set; }
         public int Year { get; set; }
 
+        private decimal _price;
+
+        public decimal Price
+        {
+            get { return _price; }
+            set
+            {
+                if(value < 0)
+                {
+                    Console.WriteLine("Price can not be negative. Keeping old value");
+                }
+                else
+                {
+                    _price = value;
+                }
+            }
+        }
+
         public Book()
         {
             Title = "Unknown";
             Author = "Unknown";
             Year = 0;
+            Price = 0;
         }
 
         public Book(string title, string author)
         {
             Title = title;
             Author = author;
-            Year = 0;
+            Price = 0;
         }
 
-        public Book(string title, string author, int year)
+        public Book(string title, string author, int year, decimal price)
         {
             Title = title;
             Author = author;
             Year = year;
+            Price = price;
         }
 
         public bool IsClassic()
@@ -40,7 +60,28 @@ namespace ConstructorsAndMethods
 
         public void PrintInfo()
         {
-            Console.WriteLine($"Title: {Title}, Author: {Author}, Year: {Year}.");
+            if(IsClassic())
+            {
+                ApplyDiscount(20);
+                Console.WriteLine($"Title: {Title}, Author: {Author}, Year: {Year}, Price: {Price:F2}.");
+            }
+            else
+            {
+                Console.WriteLine($"Title: {Title}, Author: {Author}, Year: {Year}, Price: {Price:F2}.");
+            }
+        }
+
+        public void ApplyDiscount(decimal percent)
+        {
+            if(percent > 0 && percent <= 100)
+            {
+                decimal discountAmount = (_price * percent) / 100;
+                _price -= discountAmount;
+            }
+            else
+            {
+                Console.WriteLine("Invalid discount percent");
+            }
         }
     }
 }
--- 01-CSharp-Basics/ConstructorsAndMethods/ConstructorsAndMethods/Program.cs
+++ 01-CSharp-Basics/ConstructorsAndMethods/ConstructorsAndMethods/Program.cs
@@ -2,8 +2,10 @@
 
 Book book1 = new Book();
 Book book2 = new Book("book number 2", "Author 2");
-Book book3 = new Book("book 3", "Author 3", 2000);
+Book book3 = new Book("book 3", "Author 3", 1900, 3000);
+Book book4 = new Book("book 4", "Author 4", 2000, 3000);
 
 book1.PrintInfo();
 book2.PrintInfo();
 book3.PrintInfo();
+book4.PrintInfo();
