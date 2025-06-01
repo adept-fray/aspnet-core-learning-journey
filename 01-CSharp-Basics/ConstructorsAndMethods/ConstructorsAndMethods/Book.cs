12cc567963905e58a4940800525c7e566647cc1c
adept-fray <siavash.kh41@gmail.com>
2025-06-01 17:08:44 +0200

    ConstructorsAndMethods Done.

---
+++ 01-CSharp-Basics/ConstructorsAndMethods/ConstructorsAndMethods/Book.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorsAndMethods
+{
+    internal class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+
+        public Book()
+        {
+            Title = "Unknown";
+            Author = "Unknown";
+            Year = 0;
+        }
+
+        public Book(string title, string author)
+        {
+            Title = title;
+            Author = author;
+            Year = 0;
+        }
+
+        public Book(string title, string author, int year)
+        {
+            Title = title;
+            Author = author;
+            Year = year;
+        }
+
+        public bool IsClassic()
+        {
+            return Year < 1990;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Title: {Title}, Author: {Author}, Year: {Year}.");
+        }
+    }
+}
---
+++ 01-CSharp-Basics/ConstructorsAndMethods/ConstructorsAndMethods/Program.cs
@@ -0,0 +1,9 @@
+﻿using ConstructorsAndMethods;
+
+Book book1 = new Book();
+Book book2 = new Book("book number 2", "Author 2");
+Book book3 = new Book("book 3", "Author 3", 2000);
+
+book1.PrintInfo();
+book2.PrintInfo();
+book3.PrintInfo();
