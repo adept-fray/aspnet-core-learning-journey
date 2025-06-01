355f2b480f43a9e0cb10e77716b4fbc57dd7fa66
adept-fray <siavash.kh41@gmail.com>
2025-06-01 16:46:47 +0200

    Completed ClassesAndObjects App.

---
+++ 01-CSharp-Basics/ClassesAndObjects/ClassesAndObjects/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjects
+{
+    internal class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public DateOnly Date { get; set; }
+
+        public Book(string title, string author, DateOnly date)
+        {
+            Title = title;
+            Author = author;
+            Date = date;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Title: {Title}, Author: {Author}, Date: {Date}.");
+        }
+    }
+}
---
+++ 01-CSharp-Basics/ClassesAndObjects/ClassesAndObjects/Program.cs
@@ -0,0 +1,12 @@
+﻿using ClassesAndObjects;
+
+Console.WriteLine("Hello, World!");
+
+Book book1 = new Book("book1", "book1Author", DateOnly.Parse("2025-01-01"));
+Book book2 = new Book("book2", "book2Author", DateOnly.Parse("2025-02-02"));
+Book book3 = new Book("book3", "book3Author", DateOnly.Parse("2025-03-03"));
+
+
+book1.PrintInfo();
+book2.PrintInfo();
+book3.PrintInfo();
