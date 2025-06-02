e8ec497e2121236137db72edd421465382cb2e57
adept-fray <siavash.kh41@gmail.com>
2025-06-02 15:48:51 +0200

    created MediaLibraryApp

---
+++ 01-CSharp-Basics/MediaLibraryApp/MediaLibraryApp/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaLibraryApp
+{
+    internal class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+        private decimal _price;
+
+        public decimal Price { get { return _price; } set { _price = value < 0 ? 0: value; } }
+
+        public virtual void PrintInfo()
+        {
+            Console.WriteLine($"Book has title: {Title}, author: {Author}, price{_price}");
+        }
+    }
+}
