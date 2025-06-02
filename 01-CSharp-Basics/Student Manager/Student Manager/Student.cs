85064edaa960d448fad9c3fd369f82470edbead9
adept-fray <siavash.kh41@gmail.com>
2025-06-02 16:42:21 +0200

    student class.

---
+++ 01-CSharp-Basics/Student Manager/Student Manager/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Manager
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+
+        public Student()
+        {
+            Name = "No name";
+            Age = 0;
+            Gender = "Unknown";
+        }
+
+        public Student(string name, int age, string gender)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+        }
+
+        public virtual void PrintInfo()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}, Gender: {Gender}");
+        }
+    }
+}
